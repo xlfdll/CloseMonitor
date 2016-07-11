fc947260e730c10d328eaf3dd8337b4786024569
xlfdll <xlfdll@gmail.com>
2016-07-11 11:13:59 +0800

    Version 1.0: Initial Release

---
+++ CloseMonitor/Helper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CloseMonitor
+{
+    internal static class WinAPIHelper
+    {
+        [DllImport("user32.dll")]
+        internal static extern Int32 PostMessage(IntPtr hWnd, UInt32 Msg, Int32 wParam, Int32 lParam);
+
+        internal const UInt32 WM_SYSCOMMAND = 0x0112;
+        internal const UInt32 SC_MONITORPOWER = 0xF170;
+
+        internal static readonly IntPtr HWND_BROADCAST = new IntPtr(0xFFFF);
+
+        internal const Int32 INT_MONITOROFF = 2;
+    }
+}
---
+++ CloseMonitor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CloseMonitor
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            WinAPIHelper.PostMessage(WinAPIHelper.HWND_BROADCAST, WinAPIHelper.WM_SYSCOMMAND, (Int32)WinAPIHelper.SC_MONITORPOWER, WinAPIHelper.INT_MONITOROFF);
+        }
+    }
+}
